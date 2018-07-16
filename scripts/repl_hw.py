8065120f8a1a0f0c34b58e15e44c3e59ee6aa4e7
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2018-07-16 17:24:41 +0200

    First naive try to replicate Hamilton's table 3 SGNS EngAll entry

---
+++ scripts/repl_hw.py
@@ -0,0 +1,31 @@
+import sys 
+import pandas as pd
+import numpy as np
+import scipy.stats
+
+sys.path.insert(0, "./histwords")
+from representations.sequentialembedding import SequentialEmbedding
+
+gold='datasets/HW/evaluationset_dataset0_manual.csv'
+embeddings="embeddings/eng-all_sgns"
+
+if __name__ == "__main__":
+    df = pd.read_csv(gold)
+    embeddings = SequentialEmbedding.load(embeddings, range(1800, 2000, 10))
+    for index, row in df.iterrows():
+        target = row['target']
+        ref    = row['ref']
+        gold   = int(row['gold'])
+        # offset = int(row['t'])
+        time_sims = embeddings.get_time_sims(target, ref)
+        rho, p = scipy.stats.spearmanr(time_sims.keys(), time_sims.values())
+        if p < 0.05:
+            if np.sign(rho) == np.sign(gold):
+                print "significant and correct: {target:s}/{ref:s}, {rho:0.2f}, {p:0.2f}, {series:s}".format(target=target, ref=ref, rho=rho,p=p, series=str(time_sims.values()))
+            else:
+                print "significant and incorrect: {target:s}/{ref:s}, {rho:0.2f}, {p:0.2f}, {series:s}".format(target=target, ref=ref, rho=rho,p=p, series=str(time_sims.values()))
+        else:
+            if np.sign(rho) == np.sign(gold):
+                print "insignificant and correct: {target:s}/{ref:s}, {rho:0.2f}, {p:0.2f}, {series:s}".format(target=target, ref=ref, rho=rho,p=p, series=str(time_sims.values()))
+            else:
+                print "insignificant and incorrect: {target:s}/{ref:s}, {rho:0.2f}, {p:0.2f}, {series:s}".format(target=target, ref=ref, rho=rho,p=p, series=str(time_sims.values()))
